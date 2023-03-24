69afa356ed9ea1617a90cfc072c966cf8d68c1fb
Ferdlcz <ferdlcskate@gmail.com>
2023-03-24 14:31:00 -0600

    Registro y base de datos corregido

--- back/register.php
+++ back/register.php
@@ -10,16 +10,30 @@ if (!$conn) {
   die("La conexión ha fallado: " . mysqli_connect_error());
 }
 
-$nombre = $_POST['nombre'];
-$apellidoPaterno = $_POST['apellido_paterno'];
-$apellidoMaterno = $_POST['apellido_materno'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-$telefono = $_POST['telefono'];
-$sexo = $_POST['sexo'];
-$edad = $_POST['edad'];
+if (empty($_POST['nombre']) || empty($_POST['apellido_paterno']) || empty($_POST['email']) || empty($_POST['pass'])) {
+  die("Por favor, complete todos los campos obligatorios.");
+}
 
-$sql = "CALL registro('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$email', '$pass', '$telefono', '$sexo', $edad)";
+$nombre = mysqli_real_escape_string($conn, $_POST['nombre']);
+$apellidoPaterno = mysqli_real_escape_string($conn, $_POST['apellido_paterno']);
+$apellidoMaterno = mysqli_real_escape_string($conn, $_POST['apellido_materno']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$pass = mysqli_real_escape_string($conn, $_POST['pass']);
+$telefono = mysqli_real_escape_string($conn, $_POST['telefono']);
+$sexo = mysqli_real_escape_string($conn, $_POST['sexo']);
+$edad = mysqli_real_escape_string($conn, $_POST['edad']);
+$rol = "cliente";
+
+$pass_length = strlen($pass);
+if ($pass_length < 8) {
+  die("La contraseña debe tener al menos 8 caracteres.");
+} elseif (!preg_match('/^(?=.*[A-Z])(?=.*[0-9])/', $pass)) {
+  die("La contraseña debe contener al menos una letra mayúscula y un número.");
+}
+
+$pass = password_hash($pass, PASSWORD_DEFAULT);
+
+$sql = "CALL registro('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$email', '$pass', '$telefono', '$sexo', $edad, '$rol')";
 
 if (mysqli_query($conn, $sql)) {
   echo "Usuario registrado correctamente";
--- urban.sql
+++ urban.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-03-2023 a las 05:50:42
+-- Tiempo de generación: 24-03-2023 a las 21:29:50
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -25,13 +25,19 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `registro` (IN `p_nombre` VARCHAR(50), IN `p_apellidoPaterno` VARCHAR(50), IN `p_apellidoMaterno` VARCHAR(50), IN `p_email` VARCHAR(50), IN `p_pass` VARCHAR(50), IN `p_telefono` VARCHAR(50), IN `p_sexo` VARCHAR(10), IN `p_edad` INT)   BEGIN
-    DECLARE v_id INT;
-    INSERT INTO clientes(nombre, apellidoPaterno, apellidoMaterno, email, pass, telefono, sexo, edad)
-    VALUES(p_nombre, p_apellidoPaterno, p_apellidoMaterno, p_email, p_pass, p_telefono, p_sexo, p_edad);
-    SET v_id = LAST_INSERT_ID();
-    INSERT INTO users(email, pass, id_cliente)
-    VALUES(p_email, p_pass, v_id);
+CREATE DEFINER=`root`@`localhost` PROCEDURE `registro` (IN `p_nombre` VARCHAR(50), IN `p_apellidoPaterno` VARCHAR(50), IN `p_apellidoMaterno` VARCHAR(50), IN `p_email` VARCHAR(100), IN `p_pass` VARCHAR(100), IN `p_telefono` VARCHAR(20), IN `p_sexo` ENUM('M','F','O'), IN `p_edad` INT, IN `p_rol` ENUM('cliente','admin'))   BEGIN
+  DECLARE cliente_id INT;
+  
+  INSERT INTO clientes (nombre, apellidoPaterno, apellidoMaterno, email, pass, telefono, sexo, edad, rol)
+  VALUES (p_nombre, p_apellidoPaterno, p_apellidoMaterno, p_email, p_pass, p_telefono, p_sexo, p_edad, p_rol);
+  
+  SET cliente_id = LAST_INSERT_ID();
+
+  INSERT INTO users (nombre, apellidoPaterno, email, pass, rol, id_cliente)
+  SELECT nombre, apellidoPaterno, email, pass, rol, cliente_id
+  FROM clientes
+  WHERE id = cliente_id;
+  
 END$$
 
 DELIMITER ;
@@ -46,20 +52,21 @@ CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidoPaterno` varchar(50) NOT NULL,
-  `apellidoMaterno` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
-  `telefono` varchar(50) NOT NULL,
-  `sexo` varchar(10) NOT NULL,
-  `edad` int(11) NOT NULL
+  `apellidoMaterno` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `sexo` varchar(50) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `rol` enum('cliente','admin') DEFAULT 'cliente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `email`, `pass`, `telefono`, `sexo`, `edad`) VALUES
-(6, 'Fernando', 'delacruz', 'Zapata', 'ferdlcskate@gmail.com', 'fer123', '4781023128', 'masculino', 21);
+INSERT INTO `clientes` (`id`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `email`, `pass`, `telefono`, `sexo`, `edad`, `rol`) VALUES
+(4, 'Fernando', 'delacruz', 'Zapata', 'ferdlcskate@gmail.com', '$2y$10$UtWmzl6nMCKHwLfAb1WRsuIIb7MY33LSk1KdNdP1Zd/CmTl/jFN56', '4781023128', '', 21, 'cliente');
 
 -- --------------------------------------------------------
 
@@ -69,8 +76,11 @@ INSERT INTO `clientes` (`id`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `e
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoPaterno` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `rol` enum('cliente','admin') DEFAULT 'cliente',
   `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -78,8 +88,8 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `pass`, `id_cliente`) VALUES
-(6, 'ferdlcskate@gmail.com', 'fer123', 6);
+INSERT INTO `users` (`id`, `nombre`, `apellidoPaterno`, `email`, `pass`, `rol`, `id_cliente`) VALUES
+(4, 'Fernando', 'delacruz', 'ferdlcskate@gmail.com', '$2y$10$UtWmzl6nMCKHwLfAb1WRsuIIb7MY33LSk1KdNdP1Zd/CmTl/jFN56', 'cliente', 4);
 
 --
 -- Índices para tablas volcadas
@@ -106,13 +116,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
