20e394de5688ae852b29c98c76892259eec62044
Ferdlcz <ferdlcskate@gmail.com>
2023-04-13 00:03:33 -0600

    Cambios2

--- admin/logs.php
+++ admin/logs.php
@@ -1,25 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.min.js"></script>
-    <title>Administrar productos</title>
+    
+    <title>Administrar Logs</title>
 </head>
 <body>
-    
-    <h1 class="text-center mt-4">Administrar productos</h1>
 
 
-<?php include '../back/products.php'?>
+<h1 class="text-center mt-4">Administrar Logs</h1>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  
+<?php include '../back/showLogs.php'?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="../js/ajax.js"></script> 
   <script src="../js/app.js"></script>
-
 </body>
 </html>
--- admin/paneladmin.php
+++ admin/paneladmin.php
@@ -89,6 +89,7 @@ include "../back/rolVerify.php";
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  <script src="../js/ajax.js"></script> 
   <script src="../js/index.js"></script>
   <script src="../js/app.js"></script>
 </body>
